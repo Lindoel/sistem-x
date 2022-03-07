4410dc53fd4edcb5853c9034a17e036551a75ca1
Lindoel <lindoelgomesdamasceno@gmail.com>
2022-03-07 09:41:57 -0300

    v1.0
    criação da pagina inicial de login mais cadastro de usuário

---
+++ login.js
@@ -0,0 +1,16 @@
+function tools_login_menu() {
+    document.querySelector('.tools_login_menu').style.display = 'block'
+
+}
+
+function tools_login_menu_none() {
+    document.querySelector('.tools_login_menu').style.display = 'none'
+}
+
+function register_supervisor() {
+    document.querySelector('.menu_checkboxes').style.display = 'block'
+}
+
+function new_user_cancel() {
+   document.querySelector('.menu_checkboxes').style.display = 'none'
+}
